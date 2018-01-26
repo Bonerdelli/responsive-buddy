b06968b985c069a8b37415ef62643a1a8162ca2b
bonerdelli <bonerdelli@gmail.com>
2018-01-26 13:05:22 +0300

    Update repo splitting commands

--- split-repo-1.sh
+++ split-repo-1.sh
@@ -1,8 +1,15 @@
-src/resources/less/framework
-src/resources/less/blocks.less
-src/resources/less/grid.less
-src/resources/less/mixins.less
-src/resources/less/reset.less
+# Source paths:
+# src/resources/less/framework
+# src/resources/less/blocks.less
+# src/resources/less/grid.less
+# src/resources/less/mixins.less
+# src/resources/less/reset.less
+
+# Splitting a git repository and follow directory renames
+#
+# Many thanks to @haimg, see
+# https://stackoverflow.com/questions/14759345/how-to-split-a-git-repository-and-follow-directory-renames
+# NOTE: for a fastest execution it must be done in Linux
 
 git filter-branch --prune-empty --index-filter '
     # Delete files which are NOT needed
@@ -16,8 +23,9 @@ git filter-branch --prune-empty --index-filter '
             || mv -f "$GIT_INDEX_FILE.new" "$GIT_INDEX_FILE" )
 '
 
-mkdir framework
-git mv blocks.less ./framework/
-git mv grid.less ./framework/
-git mv mixins.less ./framework/
-git mv reset.less ./framework/
+# Prune empty merge commits from history
+# NOTE: It does require GNU versions of xargs and sed
+
+git filter-branch --prune-empty --parent-filter '
+    gsed "s/-p //g" | gxargs -r git show-branch --independent | gsed "s/\</-p /g"
+'
